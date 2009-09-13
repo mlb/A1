b1994b859ec8c4c58cf10542341721affa727d79
Wouter <wouter.w@gmx.net>
2009-09-13 17:22:02 +0200

    adding again

---
+++ classes/model/a1/orm/user.php
@@ -0,0 +1,27 @@
+<?php
+
+/* 
+ * Abstract A1 Authentication User Model
+ * To be extended and completed to user's needs
+ * (see a2acldemo/classes/model/user.php for an example implementation)
+ */
+
+abstract class Model_A1_ORM_User extends ORM {
+
+	// A1 config file name
+	protected $_config = 'a1';
+
+	// user model (from config)
+	protected $_user_model;
+
+	// user columns (from config)
+	protected $_columns;
+
+	public function __construct($id = NULL)
+	{
+		$this->_columns          = Kohana::config($this->_config)->columns;
+		$this->_user_model       = Kohana::config($this->_config)->user_model;
+
+		parent::__construct($id);
+	}
+}
